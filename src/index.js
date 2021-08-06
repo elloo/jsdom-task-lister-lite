4429df8d32cac9f618c90931bb649e5ebd76b0dc
Ewe Lin Loo <ewelinloo@gmail.com>
2021-08-06 23:04:06 +1000

    Finish challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const remove = el => {el.remove()}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  let array = []
+
+  function create(task){
+    return `<li onClick=remove(this)>${task}</li>`
+  }
+
+  function createArray(task){
+    const taskObject = {id: array.length, task:task}
+    array.push(taskObject)
+    display(array)
+  }
+
+  const submitBtn = document.getElementById("submitBtn")
+  submitBtn.addEventListener('click', function(e){
+    e.preventDefault()
+
+    const taskText = document.getElementById("new-task-description").value 
+    let task = create(taskText)
+    createArray(task)
+  })
+
+  const taskList = document.getElementById("tasks")
+  function display(taskArray){
+    taskList.innerHTML = ""
+    for (const item of taskArray){
+      taskList.innerHTML += item.task
+    }
+  }
+
 });
